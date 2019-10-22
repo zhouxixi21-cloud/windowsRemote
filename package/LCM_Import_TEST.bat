08ea78edd237e43c98960f2744ec06e995dc93a3
HIEU N DOAN <hq0adm@lpcosapanar1.is.agilent.net>
2019-10-22 02:57:01 -0600

    change ansible script file

--- package/LCM_Import_TEST.bat
+++ package/LCM_Import_TEST.bat
@@ -1,10 +1,10 @@
 
-set lcmbckpath=E:\Adminutils\LCM_Jenkins\\n
+set lcmbckpath=E:\Adminutils\LCM_Jenkins\
 
-set utilitypath=E:\apps\OracleEPM\Middleware\user_projects\epmsystem_fnd\bin\n
+set utilitypath=E:\apps\OracleEPM\Middleware\user_projects\epmsystem_fnd\bin
 
-set importxmpath=E:\Adminutils\LCM_Jenkins\Import.xml\n
+set importxmpath=E:\Adminutils\LCM_Jenkins\Import.xml
 
-set logfile=E:\Adminutils\LCM_Jenkins\Importlog.txt\n
+set logfile=E:\Adminutils\LCM_Jenkins\Importlog.txt
 
 %utilitypath%\utility %importxmpath%  -b %lcmbckpath%\JenHFR_Test_201910142316 >%logfile%
