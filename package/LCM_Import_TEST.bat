b0f3595d134e6d853d7b0c38e92d088f8b605fc8
HIEU N DOAN <hq0adm@lpcosapanar1.is.agilent.net>
2019-10-22 02:02:07 -0600

    init

---
+++ package/LCM_Import_TEST.bat
@@ -0,0 +1,8 @@
+
+set lcmbckpath=E:\Adminutils\LCM_Jenkins\
+
+set utilitypath=E:\apps\OracleEPM\Middleware\user_projects\epmsystem_fnd\bin
+set importxmpath=E:\Adminutils\LCM_Jenkins\Import.xml
+
+set logfile=E:\Adminutils\LCM_Jenkins\Importlog.txt
+
